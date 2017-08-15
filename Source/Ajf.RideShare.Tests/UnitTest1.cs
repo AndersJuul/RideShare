beab8a2522dcf621af8b20c0819162d4867acb5f
AndersJuul <andersjuulsfirma@gmail.com>
2017-08-15 11:58:37 +0200

    aa

---
+++ Source/Ajf.RideShare.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Ajf.RideShare.Tests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestFixture]
+    public class UnitTest1
+    {
+
+        [TestCase]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
