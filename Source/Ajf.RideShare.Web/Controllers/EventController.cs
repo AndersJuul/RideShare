f2ee99a9eb67868d34d24e91949cd3cf9c5894f9
AndersJuul <andersjuulsfirma@gmail.com>
2017-08-24 15:49:20 +0200

    a

--- Source/Ajf.RideShare.Web/Controllers/EventController.cs
+++ Source/Ajf.RideShare.Web/Controllers/EventController.cs
@@ -20,5 +20,20 @@ namespace Ajf.RideShare.Web.Controllers
             }
         }
     }
+    [Authorize]
+    public class CarsController : Controller
+    {
+        public ActionResult Add()
+        {
+            return RedirectToAction("Index", "Home");
+            //using (var context = new ApplicationDbContext())
+            //{
+            //    var event1 = context.Events.Single(x => x.Id == id);
+
+            //    var eventDetailsViewModel = Mapper.Map<EventDetailsViewModel>(event1);
+            //    return View("EventDetails", eventDetailsViewModel);
+            //}
+        }
+    }
 
 }
