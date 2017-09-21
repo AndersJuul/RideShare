639131f7d42501e08493b70d29803c7141e75b66
AndersJuul <andersjuulsfirma@gmail.com>
2017-09-21 14:25:10 +0200

    a

--- Source/Ajf.RideShare.Tests/UnitTest1.cs
+++ Source/Ajf.RideShare.Tests/UnitTest1.cs
@@ -6,23 +6,23 @@ namespace Ajf.RideShare.Tests
     [TestFixture]
     public class UnitTest1
     {
-        //[Test]
-        //[Category("Selenium")]
-        //public void TestMethod1()
-        //{
-        //    using (var chromeDriver = new ChromeDriver())
-        //    {
-        //        chromeDriver.Manage().Window.Maximize();
-        //        chromeDriver.Navigate().GoToUrl("http://localhost/RideShare");
-        //        var emailTextBox = chromeDriver.FindElementById("Email");
-        //        emailTextBox.Clear();
-        //        emailTextBox.SendKeys("frank@email.dk");
+        [Test]
+        [Category("Selenium")]
+        public void TestMethod1()
+        {
+            //using (var chromeDriver = new ChromeDriver())
+            //{
+            //    chromeDriver.Manage().Window.Maximize();
+            //    chromeDriver.Navigate().GoToUrl("http://localhost/RideShare");
+            //    var emailTextBox = chromeDriver.FindElementById("Email");
+            //    emailTextBox.Clear();
+            //    emailTextBox.SendKeys("frank@email.dk");
 
-        //        var passwordTextBox = chromeDriver.FindElementById("Password");
-        //        passwordTextBox.Clear();
-        //        passwordTextBox.SendKeys("Frank1");
-        //    }
-        //}
+            //    var passwordTextBox = chromeDriver.FindElementById("Password");
+            //    passwordTextBox.Clear();
+            //    passwordTextBox.SendKeys("Frank1");
+            //}
+        }
 
         [Test]
         //[Category("Selenium")]
