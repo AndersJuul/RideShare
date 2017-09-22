b8794182644edd2730f79de50c6d6a33ae5229ed
AndersJuul <andersjuulsfirma@gmail.com>
2017-09-22 12:28:50 +0200

    a

--- Source/Ajf.RideShare.Tests/UnitTest1.cs
+++ Source/Ajf.RideShare.Tests/UnitTest1.cs
@@ -1,31 +1,10 @@
-﻿using System.Configuration;
-using NUnit.Framework;
-using OpenQA.Selenium.Chrome;
+﻿using NUnit.Framework;
 
 namespace Ajf.RideShare.Tests
 {
     [TestFixture]
     public class UnitTest1
     {
-        [Test]
-        [Category("Selenium")]
-        public void TestMethod1()
-        {
-            using (var chromeDriver = new ChromeDriver())
-            {
-                chromeDriver.Manage().Window.Maximize();
-                chromeDriver.Navigate().GoToUrl(ConfigurationManager.AppSettings["urlRideShareWeb"]);
-
-                var emailTextBox = chromeDriver.FindElementById("Email");
-                emailTextBox.Clear();
-                emailTextBox.SendKeys("frank@email.dk");
-
-                var passwordTextBox = chromeDriver.FindElementById("Password");
-                passwordTextBox.Clear();
-                passwordTextBox.SendKeys("Frank1");
-            }
-        }
-
         [Test]
         //[Category("Selenium")]
         public void TestMethod2()
