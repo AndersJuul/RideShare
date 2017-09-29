77360c5de4dd85ac820ff29ea004bcdacde5456d
AndersJuul <andersjuulsfirma@gmail.com>
2017-09-29 13:52:16 +0200

    a

---
+++ Source/Ajf.RideShare.Tests/UnitTests/BaseUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace Ajf.RideShare.Tests.UnitTests
+{
+    public class BaseUnitTests
+    {
+        protected Fixture _fixture;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture();
+        }
+    }
+}
---
+++ Source/Ajf.RideShare.Tests/UnitTests/EventServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Ajf.RideShare.Web.Models.ApiModels;
+using Ajf.RideShare.Web.Repositories;
+using Ajf.RideShare.Web.Services;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using Rhino.Mocks;
+
+namespace Ajf.RideShare.Tests.UnitTests
+{
+    [TestFixture]
+    public class EventServiceTests:BaseUnitTests
+    {
+        [Test]
+        public void ThatServicePassesGetResultFromRepository()
+        {
+            // Arrange
+            var eventRepository = MockRepository.GenerateMock<IEventRepository>();
+            var events = _fixture.CreateMany<Event>().ToArray();
+            eventRepository.Expect(x => x.GetEvents()).Return(events);
+            var eventService = new EventService(eventRepository);
+
+            // Act
+            var result = eventService.GetEvents();
+
+            // Assert
+            Assert.AreEqual(events, result);
+        }
+    }
+}
