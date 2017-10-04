3e510adc5b0f8077eb741456032898d37c2a40d4
AndersJuul <andersjuulsfirma@gmail.com>
2017-10-04 10:36:38 +0200

    a

--- Source/Ajf.RideShare.Web/App_Start/WebApiConfig.cs
+++ Source/Ajf.RideShare.Web/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Filters;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
@@ -16,6 +17,7 @@ namespace Ajf.RideShare.Web
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            config.Filters.Add(new AddCustomHeaderFilter());
 
             // Use camel case for JSON data.
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
@@ -28,6 +30,13 @@ namespace Ajf.RideShare.Web
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+    }
+    public class AddCustomHeaderFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            actionExecutedContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
     }
 }
