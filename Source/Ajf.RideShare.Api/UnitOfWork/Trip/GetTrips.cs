ac0242f7d4ef15cf38940cf7944d11d87084da11
AndersJuul <andersjuulsfirma@gmail.com>
2018-01-08 22:24:11 +0100

    a

--- Source/Ajf.RideShare.Api/UnitOfWork/Trip/GetTrips.cs
+++ Source/Ajf.RideShare.Api/UnitOfWork/Trip/GetTrips.cs
@@ -1,60 +1,60 @@
-﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Ajf.RideShare.Api.UnitOfWork;
-using TripGallery.Repository;
-using TripGallery.Repository.Entities;
+﻿//using AutoMapper;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Ajf.RideShare.Api.UnitOfWork;
+//using TripGallery.Repository;
+//using TripGallery.Repository.Entities;
 
-namespace TripGallery.API.UnitOfWork.Trip
-{
-    public class GetTrips : IUnitOfWork<IEnumerable<DTO.Trip>>, IDisposable
-    {
+//namespace TripGallery.API.UnitOfWork.Trip
+//{
+//    public class GetTrips : IUnitOfWork<IEnumerable<DTO.Trip>>, IDisposable
+//    {
 
-        ITripRepository _tripRepository;
-        TripContext _ctx;
-        string _ownerId;
+//        ITripRepository _tripRepository;
+//        TripContext _ctx;
+//        string _ownerId;
      
-        private GetTrips()
-        {
-           _ctx = new TripContext(@"app_data/tripstore.json");
-           _tripRepository = new TripRepository(_ctx);
-        }
+//        private GetTrips()
+//        {
+//           _ctx = new TripContext(@"app_data/tripstore.json");
+//           _tripRepository = new TripRepository(_ctx);
+//        }
 
-        public GetTrips(string ownerId)
-            : this()
-        {          
-            _ownerId = ownerId;
-        }
+//        public GetTrips(string ownerId)
+//            : this()
+//        {          
+//            _ownerId = ownerId;
+//        }
 
-        public UnitOfWorkResult<IEnumerable<DTO.Trip>> Execute()
-        {
-            var tripEntities = _tripRepository.GetTrips(_ownerId, false);
+//        public UnitOfWorkResult<IEnumerable<DTO.Trip>> Execute()
+//        {
+//            var tripEntities = _tripRepository.GetTrips(_ownerId, false);
                        
-            // return dto's
-            var dtos = Mapper.Map<IEnumerable<Repository.Entities.Trip>, IEnumerable<DTO.Trip>>(tripEntities);
-            return new UnitOfWorkResult<IEnumerable<DTO.Trip>>(dtos, UnitOfWorkStatus.Ok);
-        }
+//            // return dto's
+//            var dtos = Mapper.Map<IEnumerable<Repository.Entities.Trip>, IEnumerable<DTO.Trip>>(tripEntities);
+//            return new UnitOfWorkResult<IEnumerable<DTO.Trip>>(dtos, UnitOfWorkStatus.Ok);
+//        }
       
 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+//        public void Dispose()
+//        {
+//            Dispose(true);
+//            GC.SuppressFinalize(this);
+//        }
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                if (_tripRepository != null)
-                {
-                    _tripRepository.Dispose();
-                    _tripRepository = null;
-                } 
-            }
-        }
-    }
-}
+//        protected virtual void Dispose(bool disposing)
+//        {
+//            if (disposing)
+//            {
+//                if (_tripRepository != null)
+//                {
+//                    _tripRepository.Dispose();
+//                    _tripRepository = null;
+//                } 
+//            }
+//        }
+//    }
+//}
