2880ae657c0d8630b5a92081312df415b8a651b0
AndersJuul <andersjuulsfirma@gmail.com>
2018-01-27 19:18:25 +0100

    a

--- Source/Ajf.RideShare.Models/ApplicationDbContext.cs
+++ Source/Ajf.RideShare.Models/ApplicationDbContext.cs
@@ -1,21 +1,12 @@
 ﻿using System.Data.Entity;
-using System.Data.Entity.Migrations;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Ajf.RideShare.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext()
             : base("RideShareConnection")
         {
-        }
-
-        public static void UpdateDatabase()
-        {
-            Database.SetInitializer<ApplicationDbContext>(null);
         }
 
         public DbSet<Event> Events { get; set; }
